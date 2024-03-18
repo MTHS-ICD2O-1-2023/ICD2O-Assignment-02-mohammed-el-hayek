a09a772962240bf2d66ef80217d67c6a3715e8b5
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-03-18 03:20:22 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed El-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed El-hayek
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of Triangle.
+ */
+function calculate () {
+  // input
+  const H = parseInt(document.getElementById('H of parallelogram').value)
+  const B = parseInt(document.getElementById('B of parallelogram').value)
+
+  // process
+  const area = H * B
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
